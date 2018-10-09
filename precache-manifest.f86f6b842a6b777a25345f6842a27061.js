b361c8dabece59933c3d9e1e9447da62f680b35d
Albert <albtran@ucdavis.edu>
2018-10-08 17:37:51 -0700

    Updates

---
+++ precache-manifest.f86f6b842a6b777a25345f6842a27061.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c421f651aaf3de210fcda2fadf8b8fe",
+    "url": "/static/media/weatherApp.5c421f65.png"
+  },
+  {
+    "revision": "08d9566590c10b69acbd2ad56dc2b6fd",
+    "url": "/static/media/scenery.08d95665.jpg"
+  },
+  {
+    "revision": "a7a028491d8cd6ca3a29d5be03fa9b4f",
+    "url": "/static/media/me.a7a02849.png"
+  },
+  {
+    "revision": "9670946da62d07c7fc3df73d5446502d",
+    "url": "/static/media/checkers.9670946d.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b8afa8a655ed7ba2ea6",
+    "url": "/static/js/main.4b8afa8a.chunk.js"
+  },
+  {
+    "revision": "08824d6e0ac1d51f1c69",
+    "url": "/static/js/1.08824d6e.chunk.js"
+  },
+  {
+    "revision": "4b8afa8a655ed7ba2ea6",
+    "url": "/static/css/main.c4cfcbd4.chunk.css"
+  },
+  {
+    "revision": "1df1e36e43cfccb30a6c27e6d2d800fb",
+    "url": "/index.html"
+  }
+];
