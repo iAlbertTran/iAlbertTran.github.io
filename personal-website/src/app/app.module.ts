ce398214298054d79deb64afea3e59383a0dfdef
A-Mac <albtran@ucdavis.edu>
2019-01-18 15:24:53 -0800

    added various components

--- personal-website/src/app/app.module.ts
+++ personal-website/src/app/app.module.ts
@@ -2,13 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'About', component: AboutComponent },
+  {
+    path: 'Contact',
+    component: ContactComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: 'Home',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  { path: 'Projects', component: ProjectsComponent}
+  //{ path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
