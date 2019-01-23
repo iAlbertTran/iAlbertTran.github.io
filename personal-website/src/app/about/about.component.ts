02615de2c02fd7d714c344b23dc77841145214f8
Albert Tran <albtran@ucdavis.edu>
2019-01-22 19:14:04 -0800

    added skills section. Styling changes

--- personal-website/src/app/about/about.component.ts
+++ personal-website/src/app/about/about.component.ts
@@ -8,10 +8,29 @@ import { HelperService } from '../service/helper.service';
 })
 export class AboutComponent implements OnInit {
 
+  skills = [
+    {name: 'HTML', proficiency: 'Expert', fontAwesomeIcon: 'fab fa-html5'},
+    {name: 'CSS', proficiency: 'Expert', fontAwesomeIcon: 'fab fa-css3-alt'},
+    {name: 'JavaScript', proficiency: 'Expert', fontAwesomeIcon: 'fab fa-js-square'},
+    {name: 'Angular', proficiency: 'Advanced', fontAwesomeIcon: 'fab fa-angular'},
+    {name: 'Node.js', proficiency: 'Advanced', fontAwesomeIcon: 'fab fa-node'},
+    {name: 'C#', proficiency: 'Intermediate', fontAwesomeIcon: 'devicon-csharp-plain'},
+    {name: 'SQL', proficiency: 'Intermediate', fontAwesomeIcon: 'fas fa-code'},
+    {name: 'Bootstrap', proficiency: 'Proficient', fontAwesomeIcon: 'devicon-bootstrap-plain'},
+    {name: 'jQuery', proficiency: 'Proficient', fontAwesomeIcon: 'devicon-jquery-plain'},
+    {name: 'Java', proficiency: 'Proficient', fontAwesomeIcon: 'fab fa-java'},
+    {name: 'Swift', proficiency: 'Basic', fontAwesomeIcon: 'devicon-swift-plain'},
+    {name: 'Linux / Unix CL', proficiency: 'Proficient', fontAwesomeIcon: 'devicon-linux-plain'},
+    {name: 'Git / Github', proficiency: 'Proficient', fontAwesomeIcon: 'fab fa-github'},
+    {name: 'React', proficiency: 'Basic', fontAwesomeIcon: 'devicon-react-original'}
+
+  ];
   constructor(private _helperService: HelperService) { }
 
   ngOnInit() {
     this._helperService.setCurrentComponent('about');
-  }
 
+
+
+  }
 }
