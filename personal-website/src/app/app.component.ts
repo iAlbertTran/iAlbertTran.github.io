f117f29233d707ac9fa2d64be5392131cfa9b570
iAlbertTran <albtran@ucdavis.edu>
2019-01-31 13:23:59 -0800

    added a carat to indicate where we're at

--- personal-website/src/app/app.component.ts
+++ personal-website/src/app/app.component.ts
@@ -9,7 +9,10 @@ import { HelperService } from './service/helper.service';
 export class AppComponent implements OnInit{
 
   openHamburgerMenu = false;
-
+  moveToHome = false;
+  moveToAbout = false;
+  moveToPortfolio = false;
+  moveToContact = false;
   constructor(private _helperService: HelperService){}
 
   ngOnInit(){
@@ -22,5 +25,31 @@ export class AppComponent implements OnInit{
 
   closeNavDrawer(){
     this.openHamburgerMenu = false;
+  }
+
+  moveCaratTo(link: string){
+    this.clearCarat();
+
+    switch(link){
+      case 'home':
+        this.moveToHome = true;
+        break;
+      case 'about':
+        this.moveToAbout = true;
+        break;
+      case 'portfolio':
+        this.moveToPortfolio = true;
+        break;
+      case 'contact':
+        this.moveToContact = true;
+        break;
+    }
+  }
+
+  clearCarat(){
+    this.moveToHome = false;
+    this.moveToAbout = false;
+    this.moveToPortfolio = false;
+    this.moveToContact = false;
   }
 }
