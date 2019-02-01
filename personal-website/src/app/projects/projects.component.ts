e789027b0ffb01fa0c12eaa4d765938f80381991
iAlbertTran <albtran@ucdavis.edu>
2019-01-31 19:30:16 -0800

    added content to projects

--- personal-website/src/app/projects/projects.component.ts
+++ personal-website/src/app/projects/projects.component.ts
@@ -7,11 +7,32 @@ import { HelperService } from '../service/helper.service';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-  
+  showFunkollection = false;
+  showWeather = false;
+  showCheckers = false;
+
   constructor(private _helperService: HelperService) { }
 
   ngOnInit() {
     this._helperService.setCurrentComponent('portfolio');
+  }
+
+  showProject(project: string){
+    this.showCheckers = false;
+    this.showFunkollection = false;
+    this.showWeather = false;
+
+    switch(project){
+      case 'funkollection':
+        this.showFunkollection = true;
+        break;
+      case 'weather':
+        this.showWeather = true;
+        break;
+      case 'checkers':
+        this.showCheckers = true;
+        break;
+    }
   }
 
 }
